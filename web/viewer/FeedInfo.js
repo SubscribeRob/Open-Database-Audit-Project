a3cf38775b5cc2dba1394770d7daced98d2b03da
Rob Williams <db2robw@gmail.com>
2011-12-19 00:19:27 -0500

    Fixed copyright header issues

--- web/viewer/FeedInfo.js
+++ web/viewer/FeedInfo.js
@@ -1,26 +1,20 @@
-/*
-
-This file is part of Ext JS 4
-
-Copyright (c) 2011 Sencha Inc
-
-Contact:  http://www.sencha.com/contact
-
-GNU General Public License Usage
-This file may be used under the terms of the GNU General Public License version 3.0 as published by the Free Software Foundation and appearing in the file LICENSE included in the packaging of this file.  Please review the following information to ensure the GNU General Public License version 3.0 requirements will be met: http://www.gnu.org/copyleft/gpl.html.
-
-If you are unsure which license is appropriate for your use, please contact the sales department at http://www.sencha.com/contact.
-
-*/
-/**
- * @class FeedViewer.FeedInfo
- * @extends Ext.tab.Panel
+/* 
+ *   This file is part of the Open Database Audit Project (ODAP).
  *
- * A container class for showing a series of feed details
- * 
- * @constructor
- * Create a new Feed Info
- * @param {Object} config The config object
+ *   ODAP is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Foobar is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *   The code was developed by Rob Williams
  */
 Ext.define('FeedViewer.FeedInfo', {
     
